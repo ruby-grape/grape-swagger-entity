ruby-grape/grape-swagger-entity

commit 2615fad2cc5c7a6c32d1a068228cad36caf56661

Properly render `example` for array exposure done using another entity (#68)
* Properly render example for arrays of subresources
* Move array typing to data_type_from

This is so it matches waht happens in entity_model_type.

The main difference is we use Symbols (both keys and values) here, whereas we use
Strings there. I'm not sure the difference is intentional, but I didn't want to
introduce any actual changes in this commit.

* DRY up AttributeParser#call

The reason I'm opening this PR is because logic that was present for "standard" exposures
was accidentally left out from "using: Entity..." exposures". The point of this commit
is to hopefully prevent such omissions from being made again.

lib/grape-swagger/entity/attribute_parser.rb
@@ -10,41 +10,30 @@ module GrapeSwagger
       end
 
       def call(entity_options)
+        param = if (entity_model = model_from(entity_options))
+                  name = GrapeSwagger::Entity::Helper.model_name(entity_model, endpoint)
+                  entity_model_type(name, entity_options)
+                else
+                  data_type_from(entity_options)
+                end
+
         documentation = entity_options[:documentation]
-        entity_model = model_from(entity_options)
+        return param if documentation.nil?
 
-        if entity_model
-          name = GrapeSwagger::Entity::Helper.model_name(entity_model, endpoint)
-
-          entity_model_type = entity_model_type(name, entity_options)
-          return entity_model_type unless documentation
-
-          add_extension_documentation(entity_model_type, documentation)
-          add_array_documentation(entity_model_type, documentation) if documentation[:is_array]
-
-          entity_model_type
-        else
-          param = data_type_from(entity_options)
-          return param unless documentation
-
-          if (values = documentation[:values]) && values.is_a?(Array)
-            param[:enum] = values
-          end
-
-          if documentation[:is_array]
-            param = { type: :array, items: param }
-            add_array_documentation(param, documentation)
-          end
-
-          add_attribute_sample(param, documentation, :default)
-          add_attribute_sample(param, documentation, :example)
-
-          add_attribute_documentation(param, documentation)
-
-          add_extension_documentation(param, documentation)
-          add_discriminator_extension(param, documentation)
-          param
+        if (values = documentation[:values]) && values.is_a?(Array)
+          param[:enum] = values
         end
+
+        add_array_documentation(param, documentation) if documentation[:is_array]
+
+        add_attribute_sample(param, documentation, :default)
+        add_attribute_sample(param, documentation, :example)
+
+        add_attribute_documentation(param, documentation)
+
+        add_extension_documentation(param, documentation)
+        add_discriminator_extension(param, documentation)
+        param
       end
 
       private
@@ -79,7 +68,16 @@ module GrapeSwagger
 
         data_type = GrapeSwagger::DocMethods::DataType.call(documented_type)
 
-        document_data_type(documentation[:documentation], data_type)
+        documented_data_type = document_data_type(documentation[:documentation], data_type)
+
+        if documentation[:documentation] && documentation[:documentation][:is_array]
+          {
+            type: :array,
+            items: documented_data_type
+          }
+        else
+          documented_data_type
+        end
       end
 
       def document_data_type(documentation, data_type)

spec/grape-swagger/entity/attribute_parser_spec.rb
@@ -18,6 +18,23 @@ describe GrapeSwagger::Entity::AttributeParser do
         it { is_expected.to include('type' => 'array') }
         it { is_expected.to include('items' => { '$ref' => '#/definitions/Tag' }) }
 
+        context 'when it contains example' do
+          let(:entity_options) do
+            {
+              using: ThisApi::Entities::Tag,
+              documentation: {
+                is_array: true,
+                example: [
+                  { name: 'green' },
+                  { name: 'blue' }
+                ]
+              }
+            }
+          end
+
+          it { is_expected.to include(example: %w[green blue].map { { name: _1 } }) }
+        end
+
         context 'when it contains min_items' do
           let(:entity_options) { { using: ThisApi::Entities::Tag, documentation: { is_array: true, min_items: 1 } } }
 

commit a5ee701dd05430050a0df187c7c21db11e06dd81

Fix enum values of array attributes
This bug was introduced in
https://github.com/ruby-grape/grape-swagger-entity/pull/68

I've added a regression spec and a fix for this, along with a tiny bit
of cleanup because there seem to have been some confusion in the
naming of variables.

lib/grape-swagger/entity/attribute_parser.rb
@@ -20,10 +20,6 @@ module GrapeSwagger
         documentation = entity_options[:documentation]
         return param if documentation.nil?
 
-        if (values = documentation[:values]) && values.is_a?(Array)
-          param[:enum] = values
-        end
-
         add_array_documentation(param, documentation) if documentation[:is_array]
 
         add_attribute_sample(param, documentation, :default)
@@ -62,15 +58,15 @@ module GrapeSwagger
           !type == Array
       end
 
-      def data_type_from(documentation)
-        documented_type = documentation[:type]
-        documented_type ||= documentation[:documentation] && documentation[:documentation][:type]
+      def data_type_from(entity_options)
+        documentation = entity_options[:documentation] || {}
+        documented_type = entity_options[:type] || documentation[:type]
 
         data_type = GrapeSwagger::DocMethods::DataType.call(documented_type)
 
-        documented_data_type = document_data_type(documentation[:documentation], data_type)
+        documented_data_type = document_data_type(documentation, data_type)
 
-        if documentation[:documentation] && documentation[:documentation][:is_array]
+        if documentation[:is_array]
           {
             type: :array,
             items: documented_data_type
@@ -87,7 +83,12 @@ module GrapeSwagger
                else
                  { type: data_type }
                end
-        type[:format] = documentation[:format] if documentation&.key?(:format)
+
+        type[:format] = documentation[:format] if documentation.key?(:format)
+
+        if (values = documentation[:values]) && values.is_a?(Array)
+          type[:enum] = values
+        end
 
         type
       end

spec/grape-swagger/entity/attribute_parser_spec.rb
@@ -227,6 +227,14 @@ describe GrapeSwagger::Entity::AttributeParser do
 
           it { is_expected.to include(uniqueItems: true) }
         end
+
+        context 'when it contains values array' do
+          let(:entity_options) do
+            { documentation: { type: 'string', desc: 'Colors', is_array: true, values: %w[red blue] } }
+          end
+
+          it { is_expected.to eq(type: :array, items: { type: 'string', enum: %w[red blue] }) }
+        end
       end
 
       context 'when it is not exposed as an array' do