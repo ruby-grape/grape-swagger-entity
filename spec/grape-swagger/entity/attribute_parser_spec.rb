2b3173065ab230a8d487f72bd5cb57ebda469fde
Andrey Subbota <subbota@gmail.com>
2025-05-08 15:23:16 +0200

    Rely on request primitive check from GrapeSwagger

--- spec/grape-swagger/entity/attribute_parser_spec.rb
+++ spec/grape-swagger/entity/attribute_parser_spec.rb
@@ -295,6 +295,22 @@ describe GrapeSwagger::Entity::AttributeParser do
           it { is_expected.to include(type: 'boolean', example: example_value, default: example_value) }
         end
       end
+
+      context 'when it is exposed as a hash' do
+        let(:entity_options) { { documentation: { type: Hash, example: example_value, default: example_value } } }
+
+        context 'when the example value is true' do
+          let(:example_value) { true }
+
+          it { is_expected.to include(type: 'object', example: example_value, default: example_value) }
+        end
+
+        context 'when the example value is false' do
+          let(:example_value) { false }
+
+          it { is_expected.to include(type: 'object', example: example_value, default: example_value) }
+        end
+      end
     end
   end
 end
